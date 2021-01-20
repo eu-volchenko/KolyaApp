aa07860d8c3411969da9f957aab99ca2d808488a
eu_volchenko <eu.volchenko.kh@gmail.com>
2021-01-20 13:24:02 +0200

    Second commit

--- TestConsoleApplication/Program.cs
+++ TestConsoleApplication/Program.cs
@@ -1,12 +1,17 @@
-﻿using System;
-
-namespace TestConsoleApplication
+﻿namespace TestConsoleApplication
 {
     class Program
     {
+        private readonly string[] _nameArray = new string[] {"Kolya", "Alyona", "Zhenya", "Robin", "Chichi"};
+        private readonly string[] _cityArray = new string[] { "Kharkov", "Kiev", "Lviv", "Dnipro", "Kherson" };
+
+        // отсортировать по алфавиту
+        // сделать выборку горов которые начинаются на букву К
+        // сделать пары для имен и городов с одинаковыми индексами и сложить в дикшинари
+        // сделать все это при помощи linq
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
